870a8d3ee0f6b88e4d41b09f2e2e50b187960eaf
baha abdelatif <baha.abdelatif@gmail.com>
2019-04-07 13:52:53 +0200

    Dernieres conversions en objets et peaufinement du style

--- js/formulaire.js
+++ js/formulaire.js
@@ -7,18 +7,24 @@ class Formulaire {
   } // Fin constructor
   checkLength(){
     const formRegex = /[\W+\d+]*[^\sa-zA-Z+\-áàâäãåçéèêëíìîïñóòôöõúùûüýÿæœÁÀÂÄÃÅÇÉÈÊËÍÌÎÏÑÓÒÔÖÕÚÙÛÜÝŸÆŒ]+/m;
-    if((this.nomUtilisateur.length < 3) || (this.prenomUtilisateur.length < 3) || (formRegex.test(this.prenomUtilisateur)) || (formRegex.test(this.nomUtilisateur))){
+    if((this.nomUtilisateur.length < 3) || (this.prenomUtilisateur.length < 3) || (this.nomUtilisateur.length > 21) || (this.prenomUtilisateur.length > 21) || (formRegex.test(this.prenomUtilisateur)) || (formRegex.test(this.nomUtilisateur))){
       if(this.nomUtilisateur.length < 3){
-        this.returnedMessage += "Le nom est trop court <br/>";
+        this.returnedMessage += "Le nom est trop court (3 caracteres min.)<br/>";
+      }
+      if(this.nomUtilisateur.length > 21){
+        this.returnedMessage += "Le nom est trop long (21 caracteres max.)<br/>";
       }
       if(this.prenomUtilisateur.length < 3){
-        this.returnedMessage += "Le prénom est trop court <br/>";
+        this.returnedMessage += "Le prénom est trop court (3 caracteres min.)<br/>";
+      }
+      if(this.prenomUtilisateur.length > 21){
+        this.returnedMessage += "Le prénom est trop long (21 caracteres max.)<br/>";
       }
       if(formRegex.test(this.prenomUtilisateur)){
-        this.returnedMessage += "Le prénom contient des caractères non autorisés. <br/>";
+        this.returnedMessage += "Le prénom contient des caractères non autorisés.<br/>";
       }
       if(formRegex.test(this.nomUtilisateur)){
-        this.returnedMessage += "Le nom contient des caractères non autorisés. <br/>";
+        this.returnedMessage += "Le nom contient des caractères non autorisés.<br/>";
       }
       return false
     }else{
